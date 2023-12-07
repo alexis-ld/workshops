6953348e098dca234790a333d36bd94ba86191f1
SPANIER Nathan <spaniernathan67@gmail.com>
2023-12-07 15:45:49 +0100

    implement router specs

---
+++ apps/recipes-api/src/recipes/recipes.router.spec.ts
@@ -0,0 +1,26 @@
+import { recipesRouter } from './recipes.router';
+import { createTestingClient } from '../testing/create-testing-client';
+import { register } from '../di';
+import { RECIPES_REPOSITORY_TOKEN } from './recipes.repository';
+import { RecipesRepositoryFake } from './recipes.repository.fake';
+
+describe(recipesRouter.name, () => {
+  it('should get recipes', async () => {
+    const { client } = setUp();
+
+    const response = await client.get('/recipes');
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.items).toContainEqual(
+      expect.objectContaining({
+        id: 'rec_1',
+      })
+    );
+  });
+});
+
+function setUp() {
+  const fakeRepo = new RecipesRepositoryFake();
+  register(RECIPES_REPOSITORY_TOKEN, { useValue: fakeRepo });
+  return createTestingClient(recipesRouter);
+}
