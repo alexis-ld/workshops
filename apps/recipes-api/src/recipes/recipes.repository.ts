1c6c96487c2633cc1b86fad3f412490bb53ee7d0
SPANIER Nathan <spaniernathan67@gmail.com>
2023-12-07 15:41:33 +0100

    implement repository

--- apps/recipes-api/src/recipes/recipes.repository.ts
+++ apps/recipes-api/src/recipes/recipes.repository.ts
@@ -1,6 +1,27 @@
 import {Recipe} from './recipe';
-
+import {delay} from 'tsyringe';
+import {JSONSyncPreset} from 'lowdb/lib/node';
 
 export interface RecipesRepository {
   getRecipes(): Promise<Recipe[]>;
 }
+
+class GreetingsRepositoryImpl {
+  private _db = JSONSyncPreset('greetings.db.json', {
+    recipes: [],
+  });
+
+  constructor() {
+    this._db.write();
+  }
+
+  async getRecipes(): Promise<Recipe[]> {
+    this._db.read();
+    return this._db.data.recipes;
+  }
+}
+
+
+export const RECIPES_REPOSITORY_TOKEN = delay<RecipesRepository>(
+  () => GreetingsRepositoryImpl
+);
